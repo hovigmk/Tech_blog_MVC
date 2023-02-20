2b56104a33a22a8a7bf4e21a716eae24acca5368
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-02-20 12:06:43 -0500

    fixed the display

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,4 +9,11 @@ module.exports = {
       new Date(date).getFullYear()
     }`;
   },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  },
 };
