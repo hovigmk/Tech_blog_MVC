fb75424d9517d3eccb3bf69c0478bacf05ba48af
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-03-01 18:59:46 -0500

    getting a SQL error

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -5,21 +5,10 @@ async function deleteFormHandler(event) {
     window.location.toString().split('/').length - 1
   ];
 
-  const response = await fetch(`/api/post/${id}`, {
+  await fetch(`/api/post/${id}`, {
     method: 'DELETE',
-    body: JSON.stringify({
-      post_id: id,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
   });
-
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
-  }
+  document.location.replace('/dashboard');
 }
 
 document
